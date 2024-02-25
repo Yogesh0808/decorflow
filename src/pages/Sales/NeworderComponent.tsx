8beb0247d0a15c649f350f2d2a3b81a21c2e8752
YOGESH P <95202606+Yogesh0808@users.noreply.github.com>
2024-02-25 12:49:19 +0530

    Fixed Double Close Button

--- src/pages/Sales/NeworderComponent.tsx
+++ src/pages/Sales/NeworderComponent.tsx
@@ -247,29 +247,10 @@ const CustomerTable = () => {
             </div>
           </div>
 
-          {showModal && (
+        {showModal && (
             <div className="fixed inset-0 flex items-center justify-center z-50 shadow-xl rounded-lg">
-              <div className="absolute shadow-xl rounded-xl">
+              <div className="absolute rounded-xl">
                 {/* Close button */}
-                <button
-                  className="absolute top-2 right-2"
-                  onClick={handleCloseModal}
-                >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="h-6 w-6"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth={2}
-                      d="M6 18L18 6M6 6l12 12"
-                    />
-                  </svg>
-                </button>
                 <div className="modal-content">
                   {/* Render the form for the selected product */}
                   {renderProductForm()}
