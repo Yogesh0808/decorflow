9b8851fdac54996aac023576f1d643087fa2086c
YOGESH P <95202606+Yogesh0808@users.noreply.github.com>
2024-02-27 10:16:03 +0530

    Added Axios

--- src/pages/Sales/orderForm/CurtainsForm.tsx
+++ src/pages/Sales/orderForm/CurtainsForm.tsx
@@ -1,56 +1,78 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import axios from "axios";
 
-// Define interface for props
 interface CurtainsFormProps {
   onCloseModal: () => void;
-  onSubmit: (formData: any) => void;
+  selectedCustomer: { id: string; clientName: string };
 }
 
-// Define CurtainsForm component
 const CurtainsForm: React.FC<CurtainsFormProps> = ({
   onCloseModal,
-  onSubmit,
+  selectedCustomer,
 }) => {
-  // State to hold form data
   const [formData, setFormData] = useState<any>({
-    title: '',
-    description: '',
-    size: '',
-    widthOfFabric: '',
-    noOfPieces: '',
-    noOfPanels: '',
-    modelOfStitching: '',
-    fabricName: '',
-    fabricCode: '',
+    title: "",
+    description: "",
+    size: "",
+    widthOfFabric: "",
+    noOfPieces: "",
+    noOfPanels: "",
+    modelOfStitching: "",
+    fabricName: "",
+    fabricCode: "",
     fabricImage: null,
-    tieOption: '',
-    remarks: '',
+    tieOption: "",
+    remarks: "",
   });
 
-  // Function to handle input changes
   const handleInputChange = (
-    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
   ) => {
     const { name, value } = e.target;
-    setFormData({
-      ...formData,
+    setFormData((prevFormData) => ({
+      ...prevFormData,
       [name]: value,
-    });
+    }));
   };
 
-  // Function to handle file input change
   const handleFileInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const file = e.target.files && e.target.files[0];
-    setFormData({
-      ...formData,
-      fabricImage: file,
-    });
+    if (file) {
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        setFormData({
+          ...formData,
+          fabricImage: reader.result as string,
+        });
+      };
+      reader.readAsDataURL(file);
+    }
   };
 
-  // Function to handle form submission
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    onSubmit(formData); // Pass form data to the parent component
+    try {
+      const dataToSubmit = {
+        ...formData,
+        customerName: selectedCustomer.clientName,
+        customerId: selectedCustomer.id,
+      };
+      console.log(dataToSubmit);
+
+      /*const response = await axios.post(
+        `/api/products/${selectedCustomer.id}/Curtains`,
+        dataToSubmit
+      );*/
+
+      // Handle success response
+      console.log("Form submitted successfully:", response.data);
+
+      // Close modal or perform any other necessary actions
+      onCloseModal();
+    } catch (error) {
+      console.error("Error submitting form:", error);
+      // Handle error
+    }
   };
 
   return (
@@ -84,7 +106,7 @@ const CurtainsForm: React.FC<CurtainsFormProps> = ({
         </button>
       </div>
       <div className="overflow-auto max-h-[30rem]">
-        <form className="p-4 md:p-5" onSubmit={onSubmit}>
+        <form className="p-4 md:p-5" onSubmit={handleSubmit}>
           <div className="grid gap-4 mb-4 grid-cols-1 md:grid-cols-2">
             <div className="col-span-2">
               <label
@@ -96,6 +118,9 @@ const CurtainsForm: React.FC<CurtainsFormProps> = ({
               <input
                 type="text"
                 id="title"
+                name="title" // Add a unique name for each input field
+                value={formData.title} // Set value prop dynamically
+                onChange={handleInputChange} // Assign the onChange handler
                 className="bg-slate-50 border border-slate-300 text-slate-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-slate-600 dark:border-slate-500 dark:placeholder-slate-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
                 placeholder="Enter title"
               />
@@ -108,8 +133,12 @@ const CurtainsForm: React.FC<CurtainsFormProps> = ({
                 Description
               </label>
               <textarea
+                type="text"
                 id="description"
+                name="description"
+                value={formData.description}
                 rows={4}
+                onChange={handleInputChange}
                 className="block p-2.5 w-full text-sm text-slate-900 bg-slate-50 rounded-lg border border-slate-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-slate-600 dark:border-slate-500 dark:placeholder-slate-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                 placeholder="Write product description here"
               ></textarea>
@@ -123,7 +152,10 @@ const CurtainsForm: React.FC<CurtainsFormProps> = ({
               </label>
               <input
                 type="text"
+                value={formData.size}
                 id="size"
+                name="size"
+                onChange={(e) => handleInputChange(e)} // Add onChange event handler
                 className="bg-slate-50 border border-slate-300 text-slate-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-slate-600 dark:border-slate-500 dark:placeholder-slate-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
                 placeholder="Enter size"
               />
@@ -138,6 +170,9 @@ const CurtainsForm: React.FC<CurtainsFormProps> = ({
               <input
                 type="text"
                 id="widthOfFabric"
+                name="widthOfFabric"
+                value={formData.widthOfFabric}
+                onChange={(e) => handleInputChange(e)}
                 className="bg-slate-50 border border-slate-300 text-slate-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-slate-600 dark:border-slate-500 dark:placeholder-slate-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
                 placeholder="Enter width of fabric"
               />
@@ -152,6 +187,9 @@ const CurtainsForm: React.FC<CurtainsFormProps> = ({
               <input
                 type="text"
                 id="noOfPieces"
+                name="noOfPieces"
+                value={formData.noOfPieces}
+                onChange={(e) => handleInputChange(e)}
                 className="bg-slate-50 border border-slate-300 text-slate-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-slate-600 dark:border-slate-500 dark:placeholder-slate-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
                 placeholder="Enter number of pieces"
               />
@@ -166,6 +204,9 @@ const CurtainsForm: React.FC<CurtainsFormProps> = ({
               <input
                 type="number"
                 id="noOfPanels"
+                name="noOfPanels"
+                value={formData.noOfPanels}
+                onChange={(e) => handleInputChange(e)}
                 className="bg-slate-50 border border-slate-300 text-slate-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-slate-600 dark:border-slate-500 dark:placeholder-slate-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
                 placeholder="Enter number of panels"
               />
@@ -180,6 +221,9 @@ const CurtainsForm: React.FC<CurtainsFormProps> = ({
               <input
                 type="text"
                 id="modelOfStitching"
+                name="modelOfStitching"
+                value={formData.modelOfStitching}
+                onChange={(e) => handleInputChange(e)}
                 className="bg-slate-50 border border-slate-300 text-slate-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-slate-600 dark:border-slate-500 dark:placeholder-slate-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
                 placeholder="Enter model of stitching"
               />
@@ -197,6 +241,9 @@ const CurtainsForm: React.FC<CurtainsFormProps> = ({
               <input
                 type="text"
                 id="fabricName"
+                name="fabricName"
+                value={formData.fabricName}
+                onChange={(e) => handleInputChange(e)}
                 className="bg-slate-50 border border-slate-300 text-slate-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-slate-600 dark:border-slate-500 dark:placeholder-slate-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
                 placeholder="Enter fabric name"
               />
@@ -212,7 +259,7 @@ const CurtainsForm: React.FC<CurtainsFormProps> = ({
                 id="tieOption"
                 name="tieOption"
                 className="bg-slate-50 border border-slate-300 text-slate-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-slate-600 dark:border-slate-500 dark:placeholder-slate-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-                onChange={handleInputChange}
+                onChange={(e) => handleInputChange(e)}
                 value={formData.tieOption}
               >
                 <option value="">Select Tie Option</option>
@@ -234,7 +281,7 @@ const CurtainsForm: React.FC<CurtainsFormProps> = ({
                 className="block p-2.5 w-full text-sm text-slate-900 bg-slate-50 rounded-lg border border-slate-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-slate-600 dark:border-slate-500 dark:placeholder-slate-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                 placeholder="Add any additional remarks here"
                 value={formData.remarks}
-                onChange={handleInputChange}
+                onChange={(e) => handleInputChange(e)}
               ></textarea>
             </div>
             <div>
@@ -247,8 +294,11 @@ const CurtainsForm: React.FC<CurtainsFormProps> = ({
               <input
                 type="text"
                 id="fabricCode"
+                name="fabricCode"
+                value={formData.fabricCode}
                 className="bg-slate-50 border border-slate-300 text-slate-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-slate-600 dark:border-slate-500 dark:placeholder-slate-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
                 placeholder="Enter fabric code"
+                onChange={(e) => handleInputChange(e)}
               />
             </div>
             <div>
@@ -261,7 +311,9 @@ const CurtainsForm: React.FC<CurtainsFormProps> = ({
               <input
                 type="file"
                 id="fabricImage"
+                name="fabricImage"
                 accept="image/*"
+                onChange={handleFileInputChange} // Add onChange event handler
                 className="block p-2.5 w-full text-sm text-slate-900 bg-slate-50 rounded-lg border border-slate-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-slate-600 dark:border-slate-500 dark:placeholder-slate-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
               />
             </div>
