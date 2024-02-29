b9dec1533f2969588fdcbb659777ae19a6055b80
YOGESH P <95202606+Yogesh0808@users.noreply.github.com>
2024-02-29 20:54:17 +0530

    Minor Bugs Fixed
    For 404

--- src/pages/Customer/AddCustomer.tsx
+++ src/pages/Customer/AddCustomer.tsx
@@ -3,7 +3,7 @@ import DefaultLayout from "../../layout/DefaultLayout";
 import axios from "axios";
 import React, { useState, useEffect } from "react";
 
-axios.defaults.baseURL = 'https://cors-h05i.onrender.com';
+axios.defaults.baseURL = "https://cors-h05i.onrender.com";
 
 const getHeaders = () => {
   const username = "abinesh";
@@ -77,7 +77,7 @@ const AddCustomer = () => {
   };
 
   const handleCloseToast = () => {
-    setShowToast(false); // Hide the toast
+    setShowToast(false);
   };
 
   return (
@@ -234,7 +234,7 @@ const AddCustomer = () => {
         {showToast && (
           <div
             id="toast-success"
-            className="absolute lg:top-32 sm:top-2 right-2 flex items-center w-full max-w-xs p-2 mb-4 text-gray-500 bg-white rounded-lg shadow"
+            className="absolute sm:top-2 right-2 flex items-center w-full max-w-xs p-2 mb-4 text-gray-500 bg-white rounded-lg shadow"
             role="alert"
           >
             <div className="inline-flex items-center justify-center flex-shrink-0 w-8 h-8 text-green-500 bg-green-100 rounded-lg">
