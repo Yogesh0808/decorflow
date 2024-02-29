f6551481b00f50104edf3a8628e8639ba1a85c40
YOGESH P <95202606+Yogesh0808@users.noreply.github.com>
2024-02-29 20:16:34 +0530

    Added 404 Page
    Added Error handling Page

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,25 @@
-import { useEffect, useState } from 'react';
-import { Route, Routes, useLocation } from 'react-router-dom';
+import { useEffect, useState } from "react";
+import { Route, Routes, useLocation, useParams } from "react-router-dom";
 
-import Loader from './common/Loader';
-import PageTitle from './components/PageTitle';
-import SignIn from './pages/Authentication/SignIn';
-import SignUp from './pages/Authentication/SignUp';
-import Calendar from './pages/Calendar';
-import Chart from './pages/Chart';
-import ECommerce from './pages/Dashboard/ECommerce';
-import FormElements from './pages/Form/FormElements';
-import FormLayout from './pages/Form/FormLayout';
-import Profile from './pages/Profile';
-import Settings from './pages/Settings';
-import Tables from './pages/Tables';
-import Alerts from './pages/UiElements/Alerts';
-import Buttons from './pages/UiElements/Buttons';
-import Neworder from './pages/Sales/Neworder';
-import Vieworder from './pages/Sales/Vieworder';
-import AddCustomer from './pages/Customer/AddCustomer';
-import ViewCustomer from './pages/Customer/ViewCustomer';
+import Loader from "./common/Loader/index";
+import PageTitle from "./components/PageTitle";
+import SignIn from "./pages/Authentication/SignIn";
+import SignUp from "./pages/Authentication/SignUp";
+import Calendar from "./pages/Calendar";
+import Chart from "./pages/Chart";
+import ECommerce from "./pages/Dashboard/ECommerce";
+import FormElements from "./pages/Form/FormElements";
+import FormLayout from "./pages/Form/FormLayout";
+import Profile from "./pages/Profile";
+import Settings from "./pages/Settings";
+import Tables from "./pages/Tables";
+import Alerts from "./pages/UiElements/Alerts";
+import Buttons from "./pages/UiElements/Buttons";
+import Neworder from "./pages/Sales/Neworder";
+import Vieworder from "./pages/Sales/Vieworder";
+import AddCustomer from "./pages/Customer/AddCustomer";
+import ViewCustomer from "./pages/Customer/ViewCustomer";
+import NotFound from "./pages/NotFound";
 
 function App() {
   const [loading, setLoading] = useState<boolean>(true);
@@ -182,6 +183,7 @@ function App() {
             </>
           }
         />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </>
   );
