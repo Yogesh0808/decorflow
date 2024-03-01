d66a045c116c96088a23df652aa0b80795795fe0
YOGESH P <95202606+Yogesh0808@users.noreply.github.com>
2024-03-01 20:30:59 +0530

    Update NotFound.tsx
    Added smooth Animations

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,12 +1,32 @@
-import React from 'react';
+import React from "react";
+import "../css/404.css";
 
-const NotFound: React.FC = () => {
+const NotFound = () => {
   return (
-    <div>
-      <h1>404 - Not Found</h1>
-      <p>Sorry, the page you are looking for does not exist.</p>
-    </div>
+    <section className="page_404 flex justify-center items-center">
+      <div className="container">
+        <div className="row">
+          <div className="col-sm-12 ">
+            <div className="col-sm-10 col-sm-offset-1  text-center">
+              <div className="four_zero_four_bg">
+                <h1 className="text-center ">404</h1>
+              </div>
+
+              <div className="contant_box_404">
+                <h3 className="h2">Look like you're lost</h3>
+
+                <p>the page you are looking for not avaible!</p>
+
+                <a href="" className="link_404">
+                  Go to Home
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   );
-}
+};
 
 export default NotFound;
