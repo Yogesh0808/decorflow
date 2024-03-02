596e5334eac80af645660fd708ca7413c69f1ebe
YOGESH P <95202606+Yogesh0808@users.noreply.github.com>
2024-03-02 18:13:02 +0530

    Minor Bug Fix
    For the Class and aria hidden

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -358,8 +358,8 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
                   }
                 >
                   <svg
-                    class="w-6 h-6 text-gray-800 dark:text-white"
-                    ariaHidden="true"
+                    className="w-6 h-6 text-gray-800 dark:text-white"
+                    aria-hidden="true"
                     xmlns="http://www.w3.org/2000/svg"
                     fill="none"
                     viewBox="0 0 24 24"
