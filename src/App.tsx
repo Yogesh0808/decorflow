5937852fa8e9d5c9dff919b80883c9db7c4fe3ac
prabeen <prabeenprabu@gmail.com>
2024-03-24 15:09:32 +0530

    new axios api replaced

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import { useEffect, useState } from "react";
 import {
-  Navigate,
-  Route,
-  Routes,
-  useLocation,
-  useParams,
+    Navigate,
+    Route,
+    Routes,
+    useLocation,
+    useParams,
 } from "react-router-dom";
 import axios from "axios";
 
@@ -24,230 +24,233 @@ import NewInvoice from "./pages/Invoice/NewInvoice";
 import ViewInvoice from "./pages/Invoice/ViewInvoice";
 import PrintInvoice from "./pages/Invoice/PrintInvoice";
 
-axios.defaults.baseURL = "https://real-plum-meerkat-vest.cyclic.app";
+axios.defaults.baseURL = "https://time-glamorous-feast.glitch.me/";
 
 function App() {
-  const [loading, setLoading] = useState<boolean>(true);
-  const { pathname } = useLocation();
+    const [loading, setLoading] = useState<boolean>(true);
+    const { pathname } = useLocation();
 
-  useEffect(() => {
-    window.scrollTo(0, 0);
-  }, [pathname]);
+    useEffect(() => {
+        window.scrollTo(0, 0);
+    }, [pathname]);
 
-  useEffect(() => {
-    setTimeout(() => setLoading(false), 500);
-  }, []);
+    useEffect(() => {
+        setTimeout(() => setLoading(false), 500);
+    }, []);
 
-  return loading ? (
-    <Loader />
-  ) : (
-    <>
-      <Routes>
-        <Route
-          index
-          element={
-            <>
-              <PageTitle title="Dashboard | YHD" />
-              <ECommerce />
-            </>
-          }
-        />
-        <Route
-          path="/customer/new"
-          element={
-            <>
-              <PageTitle title="New-Customer | YHD" />
-              <AddCustomer />
-            </>
-          }
-        />
-        <Route
-          path="/customer/view"
-          element={
-            <>
-              <PageTitle title="View-Customer | YHD" />
-              <ViewCustomer />
-            </>
-          }
-        />
-        <Route
-          path="/order/new"
-          element={
-            <>
-              <PageTitle title="New-Order | YHD" />
-              <Neworder />
-            </>
-          }
-        />
-        <Route
-          path="/order/view"
-          element={
-            <>
-              <PageTitle title="View-Order | YHD" />
-              <Vieworder />
-            </>
-          }
-        />
-        <Route
-          path="/order/print"
-          element={
-            <>
-              <PageTitle title="Printing Preview | YHD" />
-              <Printorder />
-            </>
-          }
-        />
-        {/* Dispatching Routing Starts Here:)) */}
-        <Route
-          path="/dispatch/entry"
-          element={
-            <>
-              <PageTitle title="OrderEntry | YHD" />
-              <OrderEntry />
-            </>
-          }
-        />
-        <Route
-          path="/dispatch/view"
-          element={
-            <>
-              <PageTitle title="OrderEntry | YHD" />
-              <ViewEntry />
-            </>
-          }
-        />
-        <Route
-          path="/invoice/new"
-          element={
-            <>
-              <PageTitle title="Quote | YHD" />
-              <NewInvoice />
-            </>
-          }
-        />
-        <Route
-          path="/invoice/view"
-          element={
-            <>
-              <PageTitle title="Viewing Invoice | YHD" />
-              <ViewInvoice />
-            </>
-          }
-        />
-        <Route
-          path="/invoice/print"
-          element={
-            <>
-              <PageTitle title="Printing Invoice | YHD" />
-              <PrintInvoice />
-            </>
-          }
-        />
-        <Route
-          path="/settings"
-          element={
-            <>
-              <PageTitle title="Settings | TailAdmin - Tailwind CSS Admin Dashboard Template" />
-              <Settings />
-            </>
-          }
-        />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </>
-  );
-  return loading ? (
-    <Loader />
-  ) : (
-    <>
-      <Routes>
-        <Route path="*" element={<NotFound />} />
-        <Route path="/" element={<Navigate to="/analytics/dashboard" />} />
-        <Route
-          index
-          path="/analytics/dashboard"
-          element={
-            <>
-              <PageTitle title="Dashboard | YHD" />
-              <Dashboard />
-            </>
-          }
-        />
-        <Route
-          path="/analytics/performance"
-          element={
-            <>
-              <PageTitle title="Performance | YHD" />
-              <Performance />
-            </>
-          }
-        />
-        <Route
-          path="/client/new"
-          element={
-            <>
-              <PageTitle title="New-Customer | YHD" />
-              <AddCustomer />
-            </>
-          }
-        />
-        <Route
-          path="/client/view"
-          element={
-            <>
-              <PageTitle title="View-Customer | YHD" />
-              <ViewCustomer />
-            </>
-          }
-        />
-        <Route
-          path="/order/new"
-          element={
-            <>
-              <PageTitle title="New-Order | YHD" />
-              <Neworder />
-            </>
-          }
-        />
-        <Route
-          path="/order/view"
-          element={
-            <>
-              <PageTitle title="View-Order | YHD" />
-              <Vieworder />
-            </>
-          }
-        />
-        <Route
-          path="/order/print"
-          element={
-            <>
-              <PageTitle title="Printing Preview | YHD" />
-              <Printorder />
-            </>
-          }
-        />
-        <Route
-          path="/dispatch/entry"
-          element={
-            <>
-              <PageTitle title="OrderEntry | YHD" />
-              <OrderEntry />
-            </>
-          }
-        />
-        <Route
-          path="/dispatch/view"
-          element={
-            <>
-              <PageTitle title="OrderEntry | YHD" />
-              <ViewEntry />
-            </>
-          }
-        />
-      </Routes>
-    </>
-  );
+    return loading ? (
+        <Loader />
+    ) : (
+        <>
+            <Routes>
+                <Route
+                    index
+                    element={
+                        <>
+                            <PageTitle title="Dashboard | YHD" />
+                            <ECommerce />
+                        </>
+                    }
+                />
+                <Route
+                    path="/customer/new"
+                    element={
+                        <>
+                            <PageTitle title="New-Customer | YHD" />
+                            <AddCustomer />
+                        </>
+                    }
+                />
+                <Route
+                    path="/customer/view"
+                    element={
+                        <>
+                            <PageTitle title="View-Customer | YHD" />
+                            <ViewCustomer />
+                        </>
+                    }
+                />
+                <Route
+                    path="/order/new"
+                    element={
+                        <>
+                            <PageTitle title="New-Order | YHD" />
+                            <Neworder />
+                        </>
+                    }
+                />
+                <Route
+                    path="/order/view"
+                    element={
+                        <>
+                            <PageTitle title="View-Order | YHD" />
+                            <Vieworder />
+                        </>
+                    }
+                />
+                <Route
+                    path="/order/print"
+                    element={
+                        <>
+                            <PageTitle title="Printing Preview | YHD" />
+                            <Printorder />
+                        </>
+                    }
+                />
+                {/* Dispatching Routing Starts Here:)) */}
+                <Route
+                    path="/dispatch/entry"
+                    element={
+                        <>
+                            <PageTitle title="OrderEntry | YHD" />
+                            <OrderEntry />
+                        </>
+                    }
+                />
+                <Route
+                    path="/dispatch/view"
+                    element={
+                        <>
+                            <PageTitle title="OrderEntry | YHD" />
+                            <ViewEntry />
+                        </>
+                    }
+                />
+                <Route
+                    path="/invoice/new"
+                    element={
+                        <>
+                            <PageTitle title="Quote | YHD" />
+                            <NewInvoice />
+                        </>
+                    }
+                />
+                <Route
+                    path="/invoice/view"
+                    element={
+                        <>
+                            <PageTitle title="Viewing Invoice | YHD" />
+                            <ViewInvoice />
+                        </>
+                    }
+                />
+                <Route
+                    path="/invoice/print"
+                    element={
+                        <>
+                            <PageTitle title="Printing Invoice | YHD" />
+                            <PrintInvoice />
+                        </>
+                    }
+                />
+                <Route
+                    path="/settings"
+                    element={
+                        <>
+                            <PageTitle title="Settings | TailAdmin - Tailwind CSS Admin Dashboard Template" />
+                            <Settings />
+                        </>
+                    }
+                />
+                <Route path="*" element={<NotFound />} />
+            </Routes>
+        </>
+    );
+    return loading ? (
+        <Loader />
+    ) : (
+        <>
+            <Routes>
+                <Route path="*" element={<NotFound />} />
+                <Route
+                    path="/"
+                    element={<Navigate to="/analytics/dashboard" />}
+                />
+                <Route
+                    index
+                    path="/analytics/dashboard"
+                    element={
+                        <>
+                            <PageTitle title="Dashboard | YHD" />
+                            <Dashboard />
+                        </>
+                    }
+                />
+                <Route
+                    path="/analytics/performance"
+                    element={
+                        <>
+                            <PageTitle title="Performance | YHD" />
+                            <Performance />
+                        </>
+                    }
+                />
+                <Route
+                    path="/client/new"
+                    element={
+                        <>
+                            <PageTitle title="New-Customer | YHD" />
+                            <AddCustomer />
+                        </>
+                    }
+                />
+                <Route
+                    path="/client/view"
+                    element={
+                        <>
+                            <PageTitle title="View-Customer | YHD" />
+                            <ViewCustomer />
+                        </>
+                    }
+                />
+                <Route
+                    path="/order/new"
+                    element={
+                        <>
+                            <PageTitle title="New-Order | YHD" />
+                            <Neworder />
+                        </>
+                    }
+                />
+                <Route
+                    path="/order/view"
+                    element={
+                        <>
+                            <PageTitle title="View-Order | YHD" />
+                            <Vieworder />
+                        </>
+                    }
+                />
+                <Route
+                    path="/order/print"
+                    element={
+                        <>
+                            <PageTitle title="Printing Preview | YHD" />
+                            <Printorder />
+                        </>
+                    }
+                />
+                <Route
+                    path="/dispatch/entry"
+                    element={
+                        <>
+                            <PageTitle title="OrderEntry | YHD" />
+                            <OrderEntry />
+                        </>
+                    }
+                />
+                <Route
+                    path="/dispatch/view"
+                    element={
+                        <>
+                            <PageTitle title="OrderEntry | YHD" />
+                            <ViewEntry />
+                        </>
+                    }
+                />
+            </Routes>
+        </>
+    );
 }
 
 export default App;
