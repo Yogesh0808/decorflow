c73854d4d4fa56be6c94624fe294cf257fc98531
YOGESH P <95202606+Yogesh0808@users.noreply.github.com>
2024-03-25 17:54:35 +0530

    Updated API URL.
    replaced render to Vercel url

--- src/App.tsx
+++ src/App.tsx
@@ -24,7 +24,7 @@ import NewInvoice from "./pages/Invoice/NewInvoice";
 import ViewInvoice from "./pages/Invoice/ViewInvoice";
 import PrintInvoice from "./pages/Invoice/PrintInvoice";
 
-axios.defaults.baseURL = "https://cors-h05i.onrender.com/";
+axios.defaults.baseURL = "https://cors-abineshcodes-projects.vercel.app";
 
 function App() {
   const [loading, setLoading] = useState<boolean>(true);
